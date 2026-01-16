c617b576d3ecd720831492e839b4f37505c35eed
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-01-16 13:13:59 +0530

    Add Cart and CartItem models with complete database associations

--- src/models/index.js
+++ src/models/index.js
@@ -6,19 +6,48 @@ const Product = require("./Product");
 const ProductImage = require("./ProductImage");
 const SyncState = require("./SyncState");
 const User = require("./auth");
+const Cart = require("./Cart");
+const CartItem = require("./CartItem");
 
+// Associations
+Department.hasMany(Category, { foreignKey: "dep_code", sourceKey: "dep_code" });
+Category.belongsTo(Department, {
+  foreignKey: "department",
+  targetKey: "dep_code",
+  as: "departmentDetails",
+});
 
+Category.hasMany(SubCategory, {
+  foreignKey: "cat_code",
+  sourceKey: "cat_code",
+});
+
+SubCategory.belongsTo(Category, {
+  foreignKey: "cat_code",
+  targetKey: "cat_code",
+});
+
+// Product Relations
 Product.hasMany(ProductImage, {
   foreignKey: "product_id",
   as: "images",
   onDelete: "CASCADE",
   onUpdate: "CASCADE",
 });
-
 ProductImage.belongsTo(Product, {
   foreignKey: "product_id",
   as: "product",
 });
+
+// Cart Relations
+User.hasOne(Cart, { foreignKey: "user_id" });
+Cart.belongsTo(User, { foreignKey: "user_id" });
+
+Cart.hasMany(CartItem, { foreignKey: "cart_id" });
+CartItem.belongsTo(Cart, { foreignKey: "cart_id" });
+
+CartItem.belongsTo(Product, { foreignKey: "product_id" });
+Product.hasMany(CartItem, { foreignKey: "product_id" });
 
 module.exports = {
   sequelize,
@@ -26,7 +55,9 @@ module.exports = {
   Category,
   SubCategory,
   Product,
-  ProductImage, 
+  ProductImage,
   SyncState,
   User,
+  Cart,
+  CartItem,
 };
