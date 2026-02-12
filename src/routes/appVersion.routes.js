d32f09b37ddbebd99c95f0851b5a8754582c54b1
Sarindu Manusara Samarasekara <sarindusamarasekara@gmail.com>
2026-02-12 10:24:48 +0530

    feat: remove authentication requirement from app version check endpoint

--- src/routes/appVersion.routes.js
+++ src/routes/appVersion.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const c = require("../controllers/appVersion.controller");
-const auth = require("../middleware/auth.middleware");
 
-router.post("/check", auth, c.checkLatestVersion);
+router.post("/check", c.checkLatestVersion);
 
 module.exports = router;
