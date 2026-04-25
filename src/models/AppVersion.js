f08ab29e1c0b2ca838692970d85570e941d1af27
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-04-25 11:46:47 +0530

    Refactor AppVersion model formatting for improved readability

--- src/models/AppVersion.js
+++ src/models/AppVersion.js
@@ -4,14 +4,26 @@ const sequelize = require("../config/db");
 const AppVersion = sequelize.define(
   "app_versions",
   {
-    id: { type: DataTypes.BIGINT.UNSIGNED, primaryKey: true, autoIncrement: true },
-    platform: { type: DataTypes.STRING(20), allowNull: false, unique: true },
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    platform: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
     latest_version: { type: DataTypes.STRING(50), allowNull: false },
-    force_update: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+    force_update: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
     created_at: { type: DataTypes.DATE, allowNull: true },
     updated_at: { type: DataTypes.DATE, allowNull: true },
   },
-  { timestamps: false }
+  { timestamps: false },
 );
 
 module.exports = AppVersion;
